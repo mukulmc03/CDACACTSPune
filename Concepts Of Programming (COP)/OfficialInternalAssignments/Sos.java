f990726679fbbcc960a7f04d04d07e177eb07c75
Mukul Chikhalkar <mukulchikhalkar.mc@gmail.com>
2020-10-11 23:45:03 +0530

    Surface Area of Sphere Method

---
+++ Concepts Of Programming (COP)/OfficialInternalAssignments/Sos.java
@@ -0,0 +1,8 @@
+package cOPOfficialInternalAssignment;
+
+public class Sos {
+	public void Sos(double radius) {
+		double sa = 4 * Math.PI * radius * radius;
+		System.out.println("Surface Area of Sphere is: " +sa);
+	}
+}
