c7a0d622a259c8de8edaeb06ba2174fb76172ce2
Mukul Chikhalkar <mukulchikhalkar.mc@gmail.com>
2020-10-11 23:44:31 +0530

    Volume of Sphere Method

---
+++ Concepts Of Programming (COP)/OfficialInternalAssignments/Vos.java
@@ -0,0 +1,10 @@
+package cOPOfficialInternalAssignment;
+
+public class Vos {
+
+	public void Vos(double radius) {
+		// TODO Auto-generated method stub
+		double vol = 4 / 3 * (Math.PI * radius * radius * radius);
+		System.out.println("Volume of Sphere is: " +vol);
+	}
+}
