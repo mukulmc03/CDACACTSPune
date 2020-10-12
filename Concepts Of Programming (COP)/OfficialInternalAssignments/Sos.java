c12c6a7f5c437b93595543d668061492fa4376b0
Mukul Chikhalkar <mukulchikhalkar.mc@gmail.com>
2020-10-12 09:33:45 +0530

    Update Sos.java

--- Concepts Of Programming (COP)/OfficialInternalAssignments/Sos.java
+++ Concepts Of Programming (COP)/OfficialInternalAssignments/Sos.java
@@ -1,7 +1,9 @@
 package cOPOfficialInternalAssignment;
 
 public class Sos {
+	double radius;
 	public void Sos(double radius) {
+		this.radius = radius;
 		double sa = 4 * Math.PI * radius * radius;
 		System.out.println("Surface Area of Sphere is: " +sa);
 	}
